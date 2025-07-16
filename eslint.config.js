bdd69b732ee1b737e1620779f8c6d0d500632b8e
Louis Johnson <louiszn269@gmail.com>
2025-07-16 11:08:37 +0700

    fix: use correct config for svelte linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
-// eslint.config.js
-import prettier from "eslint-config-prettier";
 import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
@@ -11,26 +10,43 @@ const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
-
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs.recommended,
 	prettier,
 	...svelte.configs.prettier,
-
 	{
+		plugins: {
+			svelte,
+		},
+		settings: {},
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node },
 		},
 		rules: {
 			"no-undef": "off",
+			semi: ["error", "always"],
 		},
 	},
-
 	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			globals: { ...globals.node },
+			parser: ts.parser,
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	},
+	{
+		plugins: {
+			svelte,
+		},
+		rules: {
+			...svelte.configs.recommended.rules,
+		},
 		files: ["apps/web/**/*.svelte", "apps/web/**/*.svelte.ts"],
 		languageOptions: {
-			parser: ts.parser,
+			parser: svelte.parser,
 			parserOptions: {
 				projectService: true,
 				extraFileExtensions: [".svelte"],
