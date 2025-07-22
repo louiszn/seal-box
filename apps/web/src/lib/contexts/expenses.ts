ad3a244e78252718dab6066a04dec56e09da7264
Louis Johnson <louiszn269@gmail.com>
2025-07-22 21:57:06 +0700

    chore: add expenses context

---
+++ apps/web/src/lib/contexts/expenses.ts
@@ -0,0 +1,46 @@
+import REST from "$lib/REST.js";
+import { APIRoute } from "@seal-box/enums";
+import type { APIExpense } from "@seal-box/types";
+import { getContext, setContext } from "svelte";
+import { writable, type Writable } from "svelte/store";
+
+const EXPENSES_CONTEXT = Symbol();
+
+interface ExpensesContext {
+	expenses: Writable<APIExpense[]>;
+	init: () => Promise<void>;
+	ready: Writable<boolean>;
+}
+
+export function createExpensesContext() {
+	const expenses = writable<APIExpense[]>([]);
+	const ready = writable<boolean>(false);
+
+	const init = async () => {
+		ready.set(false);
+
+		const [data, error] = await REST.get<APIExpense[]>(APIRoute.GetExpenses);
+
+		if (error) {
+			ready.set(true);
+			return;
+		}
+
+		expenses.set(data);
+		ready.set(true);
+	};
+
+	const context: ExpensesContext = {
+		expenses,
+		ready,
+		init,
+	};
+
+	setContext(EXPENSES_CONTEXT, context);
+
+	return context;
+}
+
+export function getExpensesContext() {
+	return getContext<ExpensesContext>(EXPENSES_CONTEXT);
+}
