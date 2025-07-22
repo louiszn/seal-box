d4c527fd88729b23e761b9866acd04ef1c1ff964
Louis Johnson <louiszn269@gmail.com>
2025-07-22 21:56:35 +0700

    chore: add incomes context

---
+++ apps/web/src/lib/contexts/incomes.ts
@@ -0,0 +1,46 @@
+import REST from "$lib/REST.js";
+import { APIRoute } from "@seal-box/enums";
+import type { APIIncome } from "@seal-box/types";
+import { getContext, setContext } from "svelte";
+import { writable, type Writable } from "svelte/store";
+
+const INCOMES_CONTEXT = Symbol();
+
+interface IncomesContext {
+	incomes: Writable<APIIncome[]>;
+	init: () => Promise<void>;
+	ready: Writable<boolean>;
+}
+
+export function createIncomesContext() {
+	const incomes = writable<APIIncome[]>([]);
+	const ready = writable<boolean>(false);
+
+	const init = async () => {
+		ready.set(false);
+
+		const [data, error] = await REST.get<APIIncome[]>(APIRoute.GetIncomes);
+
+		if (error) {
+			ready.set(true);
+			return;
+		}
+
+		incomes.set(data);
+		ready.set(true);
+	};
+
+	const context: IncomesContext = {
+		incomes,
+		ready,
+		init,
+	};
+
+	setContext(INCOMES_CONTEXT, context);
+
+	return context;
+}
+
+export function getIncomesContext() {
+	return getContext<IncomesContext>(INCOMES_CONTEXT);
+}
